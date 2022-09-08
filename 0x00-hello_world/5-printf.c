9c6dd3aea50e4097252132cb6c3fed47c5c072ec
ffekirnew <ffekirnew0808@gmail.com>
2022-09-08 15:15:59 +0300

    created 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point to the program
+ *Return: Always 0 (successful completion)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

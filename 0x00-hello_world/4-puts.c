6591f5ad6ca92a76442fb8d7988b93a265aa5532
ffekirnew <ffekirnew0808@gmail.com>
2022-09-08 15:12:52 +0300

    created 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point to the program
+ *Return: Always 0 (successful completion)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

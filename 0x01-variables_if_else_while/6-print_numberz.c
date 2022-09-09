117b028a64e136cec2369b7ac0039363a2a62081
ffekirnew <ffekirnew0808@gmail.com>
2022-09-09 15:58:35 +0300

    created 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point to the program
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
+}

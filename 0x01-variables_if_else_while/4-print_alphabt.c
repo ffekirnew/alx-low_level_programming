c7e41d0790d35ca7c2d6d87de8fd6f941847f49c
ffekirnew <ffekirnew0808@gmail.com>
2022-09-09 15:35:23 +0300

    created 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point to the program
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 122; a++)
+	{
+		if (a != 101 && a != 113)
+		{
+			printf("%c", a);
+		}
+	}
+	printf("%c\n", a);
+	return (0);
+}

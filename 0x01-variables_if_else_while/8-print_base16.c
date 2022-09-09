0025f36d4979a65f3577e15b95765ae0f25158cc
ffekirnew <ffekirnew0808@gmail.com>
2022-09-09 16:05:28 +0300

    created 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point to the program
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	for (a = 97; a < 103; a++)
+        {
+                putchar(a);
+        }
+	putchar(10);
+	return (0);
+}

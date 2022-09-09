d6908bf0089cdcb7fe32353b28ef039f76d338cf
ffekirnew <ffekirnew0808@gmail.com>
2022-09-09 15:25:52 +0300

    created 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point to the program
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 122; a++)
+	{
+		printf("%c", a);
+	}
+	printf("%c\n", a);
+}

ab63f645bd5684194c08004ebbabcd022bcbe229
ffekirnew <ffekirnew0808@gmail.com>
2022-09-13 21:07:39 +0300

    created the files for task number 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of the two numbers passed
+ * @a: any number
+ * @b: any second number
+ *
+ * Return: An int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

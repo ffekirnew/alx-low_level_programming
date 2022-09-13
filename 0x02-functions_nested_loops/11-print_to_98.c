c24db3e0b29a0752e37d735a0ac42a90a8ce5701
ffekirnew <ffekirnew0808@gmail.com>
2022-09-13 21:24:28 +0300

    changed 11-print_to_98.c so that it conforms to the betty documentation style

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,19 +2,19 @@
 #include <stdio.h>
 
 /**
- * _abs - returns the numbers between the passed n and the number 98
+ * print_to_98 - returns the numbers between the passed n and the number 98
  * @n: any number
  *
  * Return: void
  */
- void print_to_98(int n)
- {
- 	int start, factor;
+void print_to_98(int n)
+{
+	int start, factor;
 
 	if (n != 98)
 	{
 		start = n;
-		factor = (n < 98) ? 1 : -1 ;
+		factor = (n < 98) ? 1 : -1;
 		while (start != 98)
 		{
 			printf("%d, ", start);
@@ -22,4 +22,4 @@
 		}
 	}
 	printf("%d\n", 98);
- }
+}

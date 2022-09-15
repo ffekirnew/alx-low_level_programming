ee198da25724a92b6886e8dcd35b62925b531000
ffekirnew <ffekirnew0808@gmail.com>
2022-09-15 22:04:03 +0300

    created the files for task number 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle onto the terminal
+ * @size: the height and width of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int height, space, hashtag;
+	
+	if (size > 0)
+	{
+		for (height = 0; height < size; height++)
+		{
+			for (space = size - height - 1; space > 0; space--)
+			{
+				_putchar(' ');
+			}
+			for (hashtag = height + 1; hashtag > 0; hashtag--)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

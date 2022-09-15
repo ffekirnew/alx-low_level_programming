e1fd0b5622c22a34e1ce7c460bebb056f2d926ca
ffekirnew <ffekirnew0808@gmail.com>
2022-09-15 20:15:06 +0300

    added the files to task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point to the program
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar(10);
+	return (0);
+}

76957e7a1342396422113bc6a78faad194ff874d
ffekirnew <ffekirnew0808@gmail.com>
2022-09-15 19:52:58 +0300

    added the files for task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if the inputed charachter is and uppercase or a lower case
+ * @c: any charachter c
+ *
+ * Return: 1 for success, 0 for failure
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 59)
+	{
+		return (1);
+	}
+	return (0);
+}

0350827c729bf84959832dbe5f55419e435ce57c
ffekirnew <ffekirnew0808@gmail.com>
2022-09-15 21:33:10 +0300

    added the file for task number 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 0 - 100 following fizz buzz rules
+ *
+ * Return - Always 0
+ */
+int main(void)
+{
+	int counter;
+
+	for (counter = 1; counter < 101; counter++)
+	{
+		if (counter % 3 == 0 && counter % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (counter % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (counter % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", counter);
+		}
+		printf(" ");
+	}
+	return (0);
+}

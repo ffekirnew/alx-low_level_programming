8cca90b35b40953d001972f7c284ab427bf26e6f
ffekirnew <ffekirnew0808@gmail.com>
2022-09-19 20:15:54 +0300

    added the file for task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * rev_string - prints the inputted string in reverse
+ * @s: any string poitner
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char reverse[500];
+	int length = strlen(s) - 1;
+	int index = 0;
+	
+	strcpy(reverse, s);
+	while (index <= length)
+	{
+		reverse[index] = s[length - index];
+		index++;
+	}
+	strcpy(s, reverse);
+}

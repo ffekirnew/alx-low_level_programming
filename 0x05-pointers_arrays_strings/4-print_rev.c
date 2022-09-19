134b1fb79992ea5d7af46f3d9d30c2b256c5d68f
ffekirnew <ffekirnew0808@gmail.com>
2022-09-19 15:14:23 +0300

    added the files for task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the inputted string in reverse
+ * @s: any string poitner
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int size;
+
+	size = 0;
+	while (s[size] != '\0')
+	{
+		size++;
+	}
+	while (size >= 0)
+	{
+		_putchar(s[size]);
+		size--;
+	}
+	_putchar('\n');
+}

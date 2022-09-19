d8771d3feef66d68164ff97c883bcbfa580bd3a0
ffekirnew <ffekirnew0808@gmail.com>
2022-09-19 15:20:00 +0300

    updated the 4-print_rev to not print the new line unseen charachter

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,15 +10,15 @@ void print_rev(char *s)
 {
 	int size;
 
-	size = -1;
+	size = 0;
 	while (s[size] != '\0')
 	{
 		size++;
 	}
 	while (size >= 0)
 	{
-		_putchar(s[size]);
 		size--;
+		_putchar(s[size]);
 	}
 	_putchar('\n');
 }

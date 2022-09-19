7ea96bb120b0e577092f1e799a0b6954f3ca3170
ffekirnew <ffekirnew0808@gmail.com>
2022-09-19 14:23:07 +0300

    added the files for task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes in a pointer and makes the value it points to 98
+ * @n: any pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

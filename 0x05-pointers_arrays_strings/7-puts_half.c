e1be2a4baeedf20c126c32cda0153ca3d4da3555
ffekirnew <ffekirnew0808@gmail.com>
2022-09-19 16:52:38 +0300

    added the files for task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: a charachter string
+ *
+ * Return: an integer
+ */
+int _strlen(char *s)
+{
+	int size;
+
+	size = 0;
+	while ((s[size]) != '\0')
+	{
+		size++;
+	}
+	return (size);
+}
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: a charachter string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int index, size;
+
+	size = _strlen(str);
+	index = (size % 2 == 0) ? size / 2 : (size - 1) / 2;
+	while (str[index] > 0)
+	{
+		_putchar(str[index]);
+		index++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 void reset_to_98(int *n);
+void puts_half(char *str);
 void puts2(char *str);
 void rev_string(char *s);
 void print_rev(char *s);

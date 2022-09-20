cd62f9f5cdebef440c8e9933d8c30fac93bae947
ffekirnew <ffekirnew0808@gmail.com>
2022-09-20 20:52:26 +0300

    added the file for task 2

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - generates passwords for 101-crack_me
+ *
+ * Return: an integer
+ */
+int main(void)
+{
+	int a;
+
+	a = rand();
+	return (a);
+}

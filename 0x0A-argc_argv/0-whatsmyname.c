f66bee5175e8c2fcf0b5085cbf5fd565f949651d
ffekirnew <ffekirnew0808@gmail.com>
2022-09-30 13:16:54 +0300

    added the files for task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: the number of arguments passed
+ * @argv: an array containing string arguments
+ *
+ * Return - Always 0 (for success)
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this is a readme file for the project on 0x0A-argc_argv

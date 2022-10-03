acb4b6c6d2606879e8c9c5512662573f15616dc2
ffekirnew <ffekirnew0808@gmail.com>
2022-10-03 17:13:06 +0300

    fixed a betty style issue

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * free_grid - frees a grid
+ * @height: the height of the grid
+ *
+ * Return: void
+ */
 void free_grid(int **grid, int height)
 {
 	int i;

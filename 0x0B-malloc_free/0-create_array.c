a65cbc3c2c728e8fa51f7c9bbca04228cf68972c
ffekirnew <ffekirnew0808@gmail.com>
2022-10-03 14:25:54 +0300

    added files for task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array of the given size
+ * containing the char c at all of its indices
+ * @size: size of the array
+ * @c: a charachter
+ *
+ * Return: char *
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *answer;
+	unsigned int i = 0;
+
+	answer = malloc(sizeof(char) * (size + 1));
+	if (size == 0 || answer == NULL)
+	{
+		return (NULL);
+	}
+	while (i < size + 1)
+	{
+		answer[i] = c;
+		i++;
+	}
+	answer[i] = 0;
+	return (answer);
+}

65e11fec4732b3ad8ff1e97c3d179edd2c531520
ffekirnew <ffekirnew0808@gmail.com>
2022-10-06 23:43:05 +0300

    fixed some issues with size

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,12 +11,13 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	size_t size_1;
+	size_t size_1, size_2;
 	unsigned int index = 0, counter = 0;
 	char *answer;
 
 	size_1 = strlen(s1);
-	answer = malloc(size_1 + n + 1);
+	size_2 = strlen(s2);
+	answer = ((int) size_2 < (int) n) ? malloc(size_1 + size_2 + 1) : malloc(size_1 + n + 1);
 	if (answer == NULL)
 	{
 		return (NULL);

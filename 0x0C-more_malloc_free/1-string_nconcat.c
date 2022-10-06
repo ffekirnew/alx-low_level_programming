90dce693965f6eda0f9547764e16ec508bf0f73f
ffekirnew <ffekirnew0808@gmail.com>
2022-10-06 23:39:09 +0300

    added files for task 2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: length to be concatenated
+ *
+ * Return: char * (a string)
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	size_t size_1;
+	unsigned int index = 0, counter = 0;
+	char *answer;
+
+	size_1 = strlen(s1);
+	answer = malloc(size_1 + n + 1);
+	if (answer == NULL)
+	{
+		return (NULL);
+	}
+
+	while (s1[counter])
+	{
+		answer[index] = s1[counter];
+		counter++;
+		index++;
+	}
+	counter = 0;
+	while (s2[counter] && counter < n)
+	{
+		answer[index] = s2[counter];
+		counter++;
+		index++;
+	}
+	return (answer);
+}

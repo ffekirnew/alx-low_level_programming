21162fe36544b000100a4708973ba42af047cf2d
ffekirnew <ffekirnew0808@gmail.com>
2022-10-10 09:47:00 +0300

    added the file for task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_STRUCTURE
+#define DOG_STRUCTURE
+/**
+ * struct dog - holds data of dogs
+ * @name: name of the dog
+ * @age: age of the dog, with precision
+ * @owner: the name of the dog's owner
+ *
+ * Description: this struct will hold all the
+ * necessary information about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

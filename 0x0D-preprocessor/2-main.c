b48e329e55a9c832479ba11e12a46172527598f8
ffekirnew <ffekirnew0808@gmail.com>
2022-10-11 04:09:39 +0300

    added the file for task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the file it was compiled fron
+ *
+ * Return: 0 for success, 1 for failure
+ */
+int main(void)
+{
+	char *name = __FILE__;
+	if (name == NULL)
+	{
+		return (1);
+	}
+	printf("%s\n", name);
+	return (0);
+}

6374afc9d65f3f6979e5990a2a30daf3bed6fd2e
ffekirnew <ffekirnew0808@gmail.com>
2022-10-13 03:10:45 +0300

    added the files for task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+
+/**
+ * array_iterator - iterates an array of values to do action
+ * @array: the array of pointers to functions
+ * @size: the size of the array
+ * @action: the function pointer to do the work
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,5 @@
 #ifndef PROJECT_0x0F_FUNCTION_POINTERS_PROJECT_HEADER_FILE
 #define PROJECT_0x0F_FUNCTION_POINTERS_PROJECT_HEADER_FILE
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif

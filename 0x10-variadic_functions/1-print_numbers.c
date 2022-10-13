aed3033c41f7666f8dbd9cc03c85011b4df6135c
ffekirnew <ffekirnew0808@gmail.com>
2022-10-13 19:10:46 +0300

    added an additional test to detect null pointers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,15 +14,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list p;
-
-	va_start(p, n);
-	for (i = 0; i < n; i++)
+	
+	if (separator != NULL)
 	{
-		printf("%d", va_arg(p, int));
-		if (i < n - 1)
+		va_start(p, n);
+		for (i = 0; i < n; i++)
 		{
-			printf("%s", separator);
+			printf("%d", va_arg(p, int));
+			if (i < n - 1)
+			{
+				printf("%s", separator);
+			}
 		}
+		printf("\n");
 	}
-	printf("\n");
 }

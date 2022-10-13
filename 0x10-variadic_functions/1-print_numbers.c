2d0c2ba04e06ede271153427d5b2e0385187981e
ffekirnew <ffekirnew0808@gmail.com>
2022-10-13 19:08:28 +0300

    added the files for task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints the passed numbers using the
+ * string separater
+ * @separator: separator string
+ * @n: the number of the arguments passed
+ *
+ * Return: Nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list p;
+
+	va_start(p, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(p, int));
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,5 @@
 #ifndef VARIADIC_FUNCTIONS_HEADER
 #define VARIADIC_FUNCTIONS_HEADER
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif

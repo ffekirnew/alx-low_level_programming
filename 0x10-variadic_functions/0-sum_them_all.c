d48d73a70487bedefd69f88bec72b9cbe6454e3c
ffekirnew <ffekirnew0808@gmail.com>
2022-10-13 18:57:52 +0300

    added the files for task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * sum_them_all - sums the arguments passed
+ * @n: the first argument
+ *
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list ptr;
+
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+	va_end(ptr);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,4 @@
+#ifndef VARIADIC_FUNCTIONS_HEADER
+#define VARIADIC_FUNCTIONS_HEADER
+int sum_them_all(const unsigned int n, ...);
+#endif

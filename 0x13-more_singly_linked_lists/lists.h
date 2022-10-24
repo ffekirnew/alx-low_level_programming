c3295782c87e304fae37e962a2248a6258a9e06d
ffekirnew <ffekirnew0808@gmail.com>
2022-10-24 07:13:47 +0300

    added the files for task 0

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef _MORE_SINGLY_LIKED_LISTS_HEADER_
+#define _MORE_SINGLY_LIKED_LISTS_HEADER_
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/* Task 0 */
+size_t print_listint(const listint_t *h);
+
+#endif

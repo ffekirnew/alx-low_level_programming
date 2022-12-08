ddcd9993e4b95ac39e37aabe24971508784d4144
ffekirnew <ffekirnew0808@gmail.com>
2022-12-08 22:54:38 +0300

    added the file for task 2

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlisting_len - returns the length of a doubly-linked list
+ * @h: the head of the doubly-linked list
+ * 
+ * Return: size int
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}

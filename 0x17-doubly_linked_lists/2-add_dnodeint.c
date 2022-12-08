530f8b0a351520edf17b4f5222de0bc7910fb98a
ffekirnew <ffekirnew0808@gmail.com>
2022-12-09 00:52:33 +0300

    added a slight fix to the second task file

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,6 +16,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
+	if (head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;

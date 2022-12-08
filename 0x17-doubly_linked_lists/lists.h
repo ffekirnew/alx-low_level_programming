ccf11f393a231bbe6f106afd45c74df61a3a734d
ffekirnew <ffekirnew0808@gmail.com>
2022-12-08 23:01:22 +0300

    added a definition to the header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef _DOUBLY_LINKED_LIST_HEADER_
 #define _DOUBLY_LINKED_LIST_HEADER_
 
+#include <stddef.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -8,7 +9,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 typedef struct dlistint_s
 {
@@ -17,6 +18,10 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
+/* task 0*/
 size_t print_dlistint(const dlistint_t *h);
+
+/* task 1 */
+size_t dlistint_len(const dlistint_t *h);
 
 #endif

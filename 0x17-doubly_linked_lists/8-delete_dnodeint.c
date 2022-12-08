3f1a83389648107948a0cff8b947e82ac75b36a2
ffekirnew <ffekirnew0808@gmail.com>
2022-12-09 01:35:49 +0300

    fixed an edge case

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,8 +11,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int n = 0;
 	dlistint_t *prev;
-	dlistint_t *iterator = head;
+	dlistint_t *iterator = *head;
 
+	if (!index)
+	{
+		prev = iterator->next;
+		return (1);
+	}
 	while (n < index && iterator != NULL)
 	{
 		n++;
@@ -20,7 +25,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		iterator = iterator->next;
 	}
 	if (n == index)
-		prev->next = iterator->next;
+	{
+		if (iterator->next != NULL)
+			prev->next = iterator->next;
+		else
+			prev->next = NULL;
 		return (1);
+	}
 	return (-1);
 }

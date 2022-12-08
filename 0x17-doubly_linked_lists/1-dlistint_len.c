52c287cc2908392f8f7bd8e5d15bcc355d8aaf9e
ffekirnew <ffekirnew0808@gmail.com>
2022-12-08 22:58:46 +0300

    added a fix to the file for task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * dlisting_len - returns the length of a doubly-linked list
+ * dlistint_len - returns the length of a doubly-linked list
  * @h: the head of the doubly-linked list
- * 
+ *
  * Return: size int
  */
 size_t dlistint_len(const dlistint_t *h)

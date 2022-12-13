7e249e6f3c893531362ee685a4092c4b0e4dbc85
ffekirnew <ffekirnew0808@gmail.com>
2022-12-13 17:27:45 +0300

    fixed the bug and betty style issues

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,10 +17,10 @@ int create_file(const char *filename, char *text_content)
 		i++;
 	}
 
-	if (filename == NULL)
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (filename == NULL || fd == -1)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (text_content != NULL)
 		write(fd, text_content, i);
 	close(fd);

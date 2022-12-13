5acbada491fdc000573fe92509e5fff65e58816f
ffekirnew <ffekirnew0808@gmail.com>
2022-12-13 17:22:20 +0300

    added the files for task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file with the given text_content
+ * @filename: the file name
+ * @text_content: the text content
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	size_t i = 0;
+	int fd;
+
+	while (text_content[i])
+	{
+		i++;
+	}
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (text_content != NULL)
+		write(fd, text_content, i);
+	close(fd);
+	return (1);
+}
